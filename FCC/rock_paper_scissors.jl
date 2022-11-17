34ed732898cdb4ad40e22e33cc817628096d385a
Angelika <angelikavictoria@outlook.com>
2022-11-17 21:59:38 +0100

    Removed incoming changes

--- FCC/rock_paper_scissors.jl
+++ FCC/rock_paper_scissors.jl
@@ -2,36 +2,6 @@ function rock_paper_scissors()
     moves = ["stein", "saks", "papir"]
     player_score = 0
     computer_score = 0
-<<<<<<< HEAD
-=======
-    human_move = Base.prompt("Velg stein, saks eller papir")
-
-    println("Stein...")
-    sleep(1)
-
-    println("Saks...")
-    sleep(1)
-
-    println("Papir...")
-    sleep(1)
-
-        
-    if computer_move == human_move
-        println("Uavgjort, prøv igjen")
-    elseif computer_move == "stein" && human_move == "saks"
-        println("Du tapte. Stein slår saks.")
-        computer_score += 1
-    elseif computer_move == "saks" && human_move == "papir"
-        println("Du tapte. Saks slår papir.")
-        computer_score += 1
-    elseif computer_move == "papir" && human_move == "stein"
-        println("Du tapte! Papir slår stein")
-        computer_score += 1
-    else
-        println("Du vant! Datamaskinen valgte $computer_move")
-        player_score += 1
-    end    
->>>>>>> 743f52e707bb5dd33a84b92b59f4d7abc59fb336
     
     while computer_score < 5 || player_score < 5
         human_move = Base.prompt("Velg stein, saks eller papir")
