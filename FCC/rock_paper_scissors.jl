66d1c32e6372772395cfde78cbd207bb0ff5bdbd
Angelika <angelikavictoria@outlook.com>
2022-11-17 21:48:41 +0100

    You can now play until someone gets 5 points. Computer choices are random

--- FCC/rock_paper_scissors.jl
+++ FCC/rock_paper_scissors.jl
@@ -1,37 +1,45 @@
 function rock_paper_scissors()
     moves = ["stein", "saks", "papir"]
-    computer_move = moves[rand(1:3)]
     player_score = 0
     computer_score = 0
-
-    human_move = Base.prompt("Velg stein, saks eller papir")
-
-    println("Stein...")
-    sleep(1)
-
-    println("Saks...")
-    sleep(1)
-
-    println("Papir...")
-    sleep(1)
-
-        
-    if computer_move == human_move
-        println("Uavgjort, prøv igjen")
-    elseif computer_move == "stein" && human_move == "saks"
-        println("Du tapte. Stein slår saks.")
-        computer_score += 1
-    elseif computer_move == "saks" && human_move == "papir"
-        println("Du tapte. Saks slår papir.")
-        computer_score += 1
-    elseif computer_move == "papir" && human_move == "stein"
-        println("Du tapte! Papir slår stein")
-        computer_score += 1
-    else
-        println("Du vant! Datamaskinen valgte $computer_move")
-        player_score += 1
-    end    
     
-    println("Score:\n P: $player_score\n C: $computer_score\n ")
-
+    while computer_score < 5 || player_score < 5
+        human_move = Base.prompt("Velg stein, saks eller papir")
+        computer_move = moves[rand(1:3)]
+        
+        println("Stein...")
+        sleep(0.5)        
+        println("Saks...")
+        sleep(0.5)
+        println("Papir...")
+        sleep(0.5)
+    
+        if computer_move == human_move
+            println("Uavgjort, prøv igjen")
+        elseif computer_move == "stein" && human_move == "saks"
+            println("Du tapte. Stein slår saks.")
+            computer_score += 1
+        elseif computer_move == "saks" && human_move == "papir"
+            println("Du tapte. Saks slår papir.")
+            computer_score += 1
+        elseif computer_move == "papir" && human_move == "stein"
+            println("Du tapte! Papir slår stein")
+            computer_score += 1
+        elseif computer_move == "saks" && human_move == "stein"
+            println("Du vant! Stein slår saks.")
+            player_score += 1
+        elseif computer_move == "papir" && human_move == "saks"
+            println("Du vant! Saks slår papir.")
+            player_score += 1
+        elseif computer_move == "stein" && human_move == "papir"
+            println("Du vant! Papir slår stein")
+            player_score += 1
+        end
+      
+        println("Score:\n P: $player_score\n C: $computer_score\n ") 
+    
+        if computer_score == 5 || player_score == 5
+            break
+        end    
+    end 
 end
